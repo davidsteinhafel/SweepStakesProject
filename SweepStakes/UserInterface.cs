da00ee343fac00f4f4e413e50c971371b0d30d41
David steinhafel <davidsteinhafel@outlook.com>
2020-01-31 11:47:48 -0600

    added get user input method along with static userInterface class

---
+++ SweepStakes/UserInterface.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepStakes
+{
+    static class UserInterface
+    {
+        static void GetUserInput()
+        {
+            string userInput;
+            Console.ReadLine();
+            userInput = Console.ReadLine();
+        }
+    }
+}
